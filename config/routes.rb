b4d759aaa1c7f73f4f28977dfa4323c40b44a316
Guillermo <gibsonguillermoarturo@gmail.com>
2018-07-25 11:00:33 -0400

    added config index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #Config of Devise Gem
+  get 'pages/index'
+  
   root 'home#index' 
 end
