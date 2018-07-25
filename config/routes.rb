eb56404a63c435f55a701df7c542f6b1c0e6df4d
Guillermo <gibsonguillermoarturo@gmail.com>
2018-07-25 09:58:55 -0400

    Config of Devise Gem in the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #Config of Devise Gem
+  root 'home#index' 
 end
