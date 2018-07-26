d5cce97770d947d0a6dfbd36da4cb74e06c27dae
Guillermo <gibsonguillermoarturo@gmail.com>
2018-07-25 20:12:05 -0400

    added pages and Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #Config of Devise Gem
   get 'pages/index'
+
+  get 'pages/about'
+
+  get 'pages/services'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'home#index' 
+  #Config of Devise Gem  
+  root 'pages#index' 
 end
